19d3ce92a114a3b821631309510c83d5599f20f4
jazzari <passionjazz@gmail.com>
2020-07-15 16:57:36 +0200

    TDD to create action in Laps_controller

--- app/controllers/laps_controller.rb
+++ app/controllers/laps_controller.rb
@@ -5,4 +5,27 @@ class LapsController < ApplicationController
 
 	def show
 	end
+
+	def edit 
+	end
+
+	def create
+		@lap = current_user.laps.build(lap_params)
+
+		if @lap.save
+			flash[:notice] = "TimeLap was successfully created"
+			redirect_to laps_path
+		else
+			render :new
+		end
+	end
+
+	def new
+	end
+
+	private
+
+	def lap_params
+		params.require(:lap).permit(:id, :time, :setting, :hardware)
+	end
 end
--- spec/controllers/laps_controller_spec.rb
+++ spec/controllers/laps_controller_spec.rb
@@ -4,24 +4,44 @@ RSpec.describe LapsController, tyoe: :controller do
 
 	login_user
 	let(:user) { create :user }
+	let(:lap) { create :lap }
 	#let(:valid_session) { {} }
 
 	describe "GET #index" do 
 		subject { get :index }
 
 		it "should returns a success response" do 
-			build :lap 
 			expect(response).to be_successful
 		end
 	end
 
 	describe 'GET #show' do 
-		let(:lap) { create :lap }
 		subject { get :show, params: { id: lap.id } }
 
 		it "should returns a success response" do
 			subject 
 			expect(response).to be_successful
+		end
+	end
+
+	describe 'GET #edit' do 
+		subject { get :edit, params: { id: lap.id } }
+
+		it "should exist lap and returns a success response" do 
+			subject
+			expect(response).to be_successful
+		end
+
+	end
+
+	describe 'POST #create' do 
+	  
+		it "should create a new lap" do 
+			post :create, :params => { :lap => { id: 1, time: 1005, 
+				setting: "false", hardware: false } }
+
+			expect(Lap.count).to eq(1)
+			expect(Lap.first[:time]).to eq(1005)
 		end
 	end
 
