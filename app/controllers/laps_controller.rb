b6fc8fba11ca96dad5ad0e42d85aee884761104b
jazzari <passionjazz@gmail.com>
2020-07-20 18:43:22 +0200

    Formatting time output in show view

--- app/controllers/laps_controller.rb
+++ app/controllers/laps_controller.rb
@@ -1,7 +1,7 @@
 class LapsController < ApplicationController
 		before_action :set_lap, only: [:edit, :show, :update, :destroy]
 		before_action :authenticate_user!, except: [:index, :show]
-
+		before_action :formated_time, only: [:show]
 
 	def index
 		@laps = Lap.all.order(created_at: :DESC)
@@ -21,11 +21,6 @@ class LapsController < ApplicationController
 			flash[:notice] = "TimeLap was successfully created"
 			redirect_to lap_path(@lap)
 		else
-			puts @lap.time
-			puts @lap.setting
-			puts @lap.hardware
-			puts @lap.notes
-			puts @lap.user_id
 			render :new
 		end
 	end
@@ -60,6 +55,39 @@ class LapsController < ApplicationController
 	def lap_params
 		params.require(:lap).permit(:id, :t_minutes, :t_seconds, :t_millis,
 		 :setting, :hardware, :notes, :time)
+	end
+
+	def formated_time
+		@formated_time = ""
+		parser_time = @lap.time 
+
+		# format minutes
+		f_min = parser_time / 60000
+		if f_min < 10
+			@formated_time = "0" + f_min.to_s
+		else
+			@formated_time = f_min.to_s
+		end
+
+		# format seconds
+		f_sec = (parser_time - (f_min * 60000)) / 1000
+		if f_sec < 10
+			@formated_time += ":0" + "#{f_sec}"
+		else
+			@formated_time += ":" + "#{f_sec}"
+		end
+
+		# format milliseconds
+		f_mil = (parser_time - (f_min * 60000) - (f_sec * 1000))
+		if f_mil < 100 && f_mil > 9
+			@formated_time += ".0" + "#{f_mil}"
+		elsif f_mil < 10
+			@formated_time += ".00" + "#{f_mil}"
+		else
+			@formated_time += "." + "#{f_mil}"
+		end
+
+		@formated_time
 	end
 
 end
