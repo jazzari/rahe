f64c127f31fdd0d9117de8685b4cd9158578cd90
jazzari <passionjazz@gmail.com>
2020-08-03 16:42:41 +0200

    Adding resources Simulator, Track and Car to ActiveAdmin

---
+++ app/admin/tracks.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Track do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :simulator_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :simulator_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
